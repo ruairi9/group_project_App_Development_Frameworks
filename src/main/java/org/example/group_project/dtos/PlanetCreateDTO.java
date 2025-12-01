887f4257a528a9f195f39865c8e0d1697f1d0f9f
ruairi <ruairiv.walsh@mycit.ie>
2025-12-01 21:48:25 +0000

    adding in notnull and thing like this to planets

--- src/main/java/org/example/group_project/dtos/PlanetCreateDTO.java
+++ src/main/java/org/example/group_project/dtos/PlanetCreateDTO.java
@@ -2,16 +2,20 @@ package org.example.group_project.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record PlanetCreateDTO (
         @NotBlank(message = "planet name is needed")
         @NotEmpty(message = "planet name cannot be blank")
         String planetName,
+        @NotNull(message = "radius is required")
         int radiusKm,
         @NotBlank(message = "type is needed")
         @NotEmpty(message = "type cannot be blank")
         String type,
+        @NotNull(message = "mass is required")
         double massKg,
+        @NotNull(message = "orbital period is required")
         int orbitalPeriodDays)
 {
 
--- src/main/java/org/example/group_project/entities/Planet.java
+++ src/main/java/org/example/group_project/entities/Planet.java
@@ -1,5 +1,8 @@
 package org.example.group_project.entities;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +16,20 @@ public class Planet {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int planetId;
+    @NotNull
+    @Size(min = 2, max = 100)
     @Column(name = "planet_name")
     private String planetName;
+    @NotNull(message = "planet radius is required")
+    @Positive(message = "planet radius must be positive")
     private int radiusKm;
+    @NotNull(message = "planet type is required")
     private String type;
+    @NotNull(message = "planet mass is required")
+    @Positive(message = "planet mass must be positive")
     private double massKg;
+    @NotNull(message = "planet orbital period is required")
+    @Positive(message = "planet orbital period must be positive")
     private int orbitalPeriodDays;
 
 }
